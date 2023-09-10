14bb4e09a64b6c0ba103659aaeb22b6a587f3657
Felipe F. Morgado <ffmoliveira@ymail.com>
2023-09-11 00:12:21 +0200

    Communication between components

--- src/components/CommentsApp.vue
+++ src/components/CommentsApp.vue
@@ -2,28 +2,7 @@
   <div class="container">
     <h1>Commentaries</h1>
     <hr />
-    <div class="form-todo form-group">
-      <p>
-        <input
-          placeholder="Name"
-          type="text"
-          name="author"
-          class="form-control"
-          v-model="name"
-        />
-      </p>
-      <p>
-        <textarea
-          placeholder="Comment"
-          name="message"
-          class="form-control"
-          v-model="message"
-        ></textarea>
-      </p>
-      <button v-on:click="addComment" type="submit" class="btn btn-primary">
-        Comment
-      </button>
-    </div>
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
     <div class="list-group">
       <div class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="comment.id">
         <span class="comment__author">
@@ -42,30 +21,24 @@
 </template>
 
 <script>
+import FormTodo from './FormTodo.vue'
 export default {
+  components:{
+    FormTodo
+  },
   data() {
     return {
       comments: [],
-      name: "",
-      message: "",
     };
   },
   methods: {
-    addComment() {
-      if (this.message.trim() === "") {
-        return;
-      }
-      //console.log(this.comments[0].name);
-      this.comments.push({
-        name: this.name,
-        message: this.message,
-      });
-      this.name = "";
-      this.message = "";
-    },
     removeComment(index) {
       this.comments.splice(index, 1);
     },
+    addComment(comment){
+      this.comments.push(comment);
+
+    }
   },
   computed: {
     allComments() {
